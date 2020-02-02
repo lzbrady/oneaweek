a916330035dd66b2842509a121552cd4749a59b1
Luke Brady <lzbrady496@gmail.com>
2020-02-02 17:30:50 -0500

    Removed bind call

--- oneaweek/src/Contact/Contact.js
+++ oneaweek/src/Contact/Contact.js
@@ -15,7 +15,6 @@ class Contact extends Component {
     };
 
     this.setFormValue = this.setFormValue.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   setFormValue(event) {
